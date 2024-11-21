d0e6f7b6ba081357eb22118e19ca78d807564861
Daniel Lima <137017200+DanielGLima@users.noreply.github.com>
2024-11-21 11:27:08 -0600

    update, delete
    actualizacion de la estructura y eliminacion de una carpeta vacia

--- CalculadoraBasica/Calculadora.java
+++ CalculadoraBasica/Calculadora.java
@@ -2,6 +2,6 @@ package CalculadoraBasica;
 
 public class Calculadora {
     public static void main(String[] args) {
-        
+
     }
 }
--- HolaMundo/PrimerHolaMundo.java
+++ HolaMundo/PrimerHolaMundo.java
@@ -1,3 +1,5 @@
+package HolaMundo;
+
 public class PrimerHolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
