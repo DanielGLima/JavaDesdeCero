791763154e78014f49e6b27ce8dd4904767cfaa4
Daniel Lima <137017200+DanielGLima@users.noreply.github.com>
2024-12-19 00:00:03 -0600

    update

---
+++ DocumentacionPersonal/Documentacion.java
@@ -0,0 +1,41 @@
+/*
+
+*** POO ***
+Programacion Orientada a Objetos
+
+La POO es un paradigma de programacion que organiza el codigo en objetos
+los objetos son la representacion de la vida real, que contienen caracteristicas(atributos) y hacen acciones (metodos)
+
+*** Clases y Objetos ***
+Una clase es una plantilla donde se definen caracteristicas(Atributos) y acciones (metodos)
+
+un Objeto es una instancia de una clase
+
+
+*** Atributos y metodos ***
+Los atributos son propiedades de una clase (Variable)
+los metodos son las acciones que un objeto puede realizar (funciones)
+
+***Encapsulamiento***
+es la practica de proteger los atrributos con un modificador de acceso private
+la manera de acceder a ellos es con metodos publicos (getters and setters)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
